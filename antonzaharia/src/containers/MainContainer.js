acd3bcd22016139def292946182ba0329d528243
antonzaharia <zaharia_anton@yahoo.com>
2020-11-03 21:35:17 +0000

    main content fade

--- antonzaharia/src/containers/MainContainer.js
+++ antonzaharia/src/containers/MainContainer.js
@@ -1,19 +1,25 @@
-import React, { Component } from 'react'
-import Header from './Header'
-import Icons from './Icons'
+import React from "react";
+import Header from "./Header";
+import Icons from "./Icons";
+import {useSpring, animated } from 'react-spring'
 
-export default class MainContainer extends Component {
-    
-    
-    render() {
-        return (
-            <div id="wrapper">
-                <Header />
-                <Icons />
-                <footer id="footer">
-					<p className="copyright">&copy; antonzaharia.com</p>
-				</footer>
-            </div>
-        )
-    }
-}
+const MainContainer = () => {
+    const props = useSpring({
+        config: { duration: 1000 },
+        from: { opacity: 0 },
+        to: {
+          opacity: 1
+        }
+      });
+
+  return (
+    <animated.div id="wrapper" style={props}>
+      <Header />
+      <Icons />
+      <footer id="footer">
+        <p className="copyright">&copy; antonzaharia.com</p>
+      </footer>
+    </animated.div>
+  );
+};
+export default MainContainer;
