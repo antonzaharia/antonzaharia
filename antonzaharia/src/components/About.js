bccc3fef2713781633104e1a521dac0ceb05e005
antonzaharia <zaharia_anton@yahoo.com>
2020-11-03 20:37:27 +0000

    contact page finished

--- antonzaharia/src/components/About.js
+++ antonzaharia/src/components/About.js
@@ -14,7 +14,28 @@ export default function About({ close }) {
         <img src="images/pic03.jpg" alt="" />
       </span>
       <p>
-        
+        Even since I was a kid I wanted to work and make my own money because I
+        was born in a modest family in Romania with “enough” shortages.
+      </p>
+      <p>
+        Doing a couple of “jobs”, I realized that doing something just for money
+        is not that jolly. I tried to figure out something that I love to do
+        and, of course, to find a job that is about that thing. Surprising, it
+        was hard!
+      </p>
+      <p>
+        When I almost quit, going through sports, driving, cooking, video
+        editing I found out about coding. I heard something before about it, but
+        in my mind was that I need years and years of learning to become
+        successful. Being at the end of my high school I did not consider coding
+        a career, because I came to London and I had to support myself. After
+        almost 6 years of watching youtube tutorials and following free online
+        courses, I realized that this is that “something” I was searching for,
+        at my 18’s.
+		</p>
+		<p>Spending hours of my free time learning, without any
+        interest, proved to me that this has to be my career. And, here am I,
+        happy and proud of what I am doing.
       </p>
     </article>
   );
--- antonzaharia/src/components/Contact.js
+++ antonzaharia/src/components/Contact.js
@@ -6,9 +6,10 @@ export default function Contact({ close }) {
     <article id="contact">
       <h2 className="major">Contact</h2>
       <span><Link to="/" onClick={close} className="close"><i className="fa fa-times-circle" aria-hidden="true"></i></Link></span>
-      <p><a href="mailto:zahariaanton95@gmail.com">Send email</a></p>
-      <p><a href="https://m.me/Zaharia.Anton/">Message on Facebook</a></p>
-      <p><a href="slack://user?team=T02MD9XTF&id=U015HAMMTK4">Message on Slack</a></p>
+      <p><a className="contact-btn" href="tel:+447367111009"><i class="fas fa-phone"></i> Call (UK number)</a></p><br />
+      <p><a className="contact-btn" href="mailto:zahariaanton95@gmail.com"><i class="far fa-envelope"></i> Send email</a></p><br />
+      <p><a className="contact-btn" href="https://m.me/Zaharia.Anton/"><i class="fab fa-facebook-f"></i>&nbsp; Message on Facebook</a></p><br />
+      <p><a className="contact-btn" href="slack://user?team=T02MD9XTF&id=U015HAMMTK4"><i class="fab fa-slack"></i> Message on Slack</a></p><br />
     </article>
   );
 }
