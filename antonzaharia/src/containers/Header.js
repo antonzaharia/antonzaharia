b56a0240da4e9e5dee52686ced55749dcb896a19
antonzaharia <zaharia_anton@yahoo.com>
2020-11-04 16:11:56 +0000

    build folder

--- antonzaharia/src/containers/Header.js
+++ antonzaharia/src/containers/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Intro from './Intro'
+import Intro from '../components/Intro'
 import Head from '../components/Head'
 import Work from '../components/Work';
 import About from '../components/About';
