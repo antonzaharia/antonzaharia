4e22e0ed4f3bbaf78ac260620592eb62373b4c8f
antonzaharia <zaharia_anton@yahoo.com>
2021-01-17 13:25:45 +0000

    test component unmount test

--- antonzaharia/src/containers/TestContainer.js
+++ antonzaharia/src/containers/TestContainer.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
+import gsap from "gsap";
 
 export default class TestContainer extends Component {
   render() {
-    return <div>test</div>;
+    return <div className="test">test</div>;
+  }
+  componentWillUnmount() {
+    setTimeout(function () {
+      gsap.to(".test", { opacity: 0, duration: 2 });
+    }, 2000);
   }
 }
