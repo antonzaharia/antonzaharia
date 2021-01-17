ed16ff848206fb3dc40a455e3f52dbd3eb22e0a1
antonzaharia <zaharia_anton@yahoo.com>
2021-01-17 13:01:24 +0000

    test container for installing gsap

--- antonzaharia/src/App.js
+++ antonzaharia/src/App.js
@@ -1,22 +1,31 @@
 import MainContainer from "./containers/MainContainer";
+import TestContainer from "./containers/TestContainer";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import {useSpring, animated } from 'react-spring'
+import { useSpring, animated } from "react-spring";
 
 function App() {
   const props = useSpring({
     config: { duration: 1000 },
-    from: { opacity: 0, background: 'black' },
+    from: { opacity: 0, background: "black" },
     to: {
       opacity: 1,
-      background: 'black'
-    }
+      background: "black",
+    },
   });
   return (
     <>
       <Router>
         <>
           {/* <Route exact path="/" render={ routerProps => <ContentContainer {...routerProps} />}/> */}
-          <Route exact path="/" render={ routerProps => <MainContainer {...routerProps} />}/>
+          <Route
+            exact
+            path="/"
+            render={(routerProps) => <MainContainer {...routerProps} />}
+          />
+          <Route
+            path="/test"
+            render={(routerProps) => <TestContainer {...routerProps} />}
+          />
           <animated.div id="bg" style={props}></animated.div>
         </>
       </Router>
